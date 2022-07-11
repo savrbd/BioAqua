b2d40fd48458662c2d6b8abd5ff0cb169a15a526
savr <savr.bd@yandex.ru>
2022-07-11 10:05:49 +0300

    add meta and title

--- src/components/mainPage/footerMain.jsx
+++ src/components/mainPage/footerMain.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 const FooterMain = () => {
     return (
         <div className="navbar navbar-dark success mt-5 text-white p-2" style={{ backgroundColor: "#004357" }}>
-        <p>
-            8 800 333 22 33<br/>
-            Звонок по России бесплатный</p>
+        <p>8 937 468-08-70</p>
 
         </div>
     );
